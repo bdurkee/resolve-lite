25ba53173a0313116bfd7af1d61095731f1adb75
Welchd1 <dtw.welch@gmail.com>
2015-09-08 16:52:50 -0400

    added more builtin op test

--- compiler/test/edu/clemson/resolve/TestBuiltinOperators.java
+++ compiler/test/edu/clemson/resolve/TestBuiltinOperators.java
@@ -5,9 +5,7 @@ import org.junit.Test;
 import org.stringtemplate.v4.ST;
 
 public class TestBuiltinOperators extends BaseTest {
-    //Swap, assignment,
-    //do some involving structs.
-    //do some regression tests for the aliasing issue on git
+
     @Test public void testAssignAndSwap() throws Exception {
         ST facilityST = new ST(
                 "Facility T; uses Standard_Integers, Standard_Char_Strings;" +
@@ -67,5 +65,29 @@ public class TestBuiltinOperators extends BaseTest {
         String facility = facilityST.render();
         String found = execCode("T.resolve", facility, "T", false);
         Assert.assertEquals("32\n32\n32\n88\n", found);
+    }
+
+    @Test public void testStructFieldSwap() throws Exception {
+        ST facilityST = new ST(
+                "Facility T; uses Standard_Integers, Standard_Char_Strings;" +
+                        "Type Foo = Record" +
+                        "   i,j : Std_Integer_Fac :: Integer;" +
+                        "   end;" +
+                        "Operation Main(); Procedure" +
+                        "   Var F,X : Foo; Var I : Std_Integer_Fac :: Integer;" +
+                        "   F.i:=32; X.i:=88; F.j:=4; X.j:=5;" +
+                        "   Std_Integer_Fac :: Write_Line(F.i); " +
+                        "   Std_Integer_Fac :: Write_Line(F.j); " +
+                        "   Std_Integer_Fac :: Write_Line(X.i); " +
+                        "   Std_Integer_Fac :: Write_Line(X.j); " +
+                        "   F.i:=:X.i;" +
+                        "   Std_Integer_Fac :: Write_Line(F.i); " +
+                        "   Std_Integer_Fac :: Write_Line(F.j); " +
+                        "   Std_Integer_Fac :: Write_Line(X.i); " +
+                        "   Std_Integer_Fac :: Write_Line(X.j); " +
+                        "end Main; end T;");
+        String facility = facilityST.render();
+        String found = execCode("T.resolve", facility, "T", false);
+        Assert.assertEquals("32\n4\n88\n5\n88\n4\n32\n5\n", found);
     }
 }
