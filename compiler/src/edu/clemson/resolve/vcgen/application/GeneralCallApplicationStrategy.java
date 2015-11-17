1f83cbaa6f244019a9102d7deaceb0dd0e0bb436
Welchd1 <dtw.welch@gmail.com>
2015-11-16 19:09:34 -0800

    fix for pset

--- compiler/src/edu/clemson/resolve/vcgen/application/GeneralCallApplicationStrategy.java
+++ compiler/src/edu/clemson/resolve/vcgen/application/GeneralCallApplicationStrategy.java
@@ -61,13 +61,13 @@ public class GeneralCallApplicationStrategy
                 //T1.Constraint(t) /\ T3.Constraint(v) /\ T6.Constraint(y) /\
                 //postcondition
                 //TODO: Ask about these constraints
-               if (distinguishedModes.contains(p.getMode())) {
+              /* if (distinguishedModes.contains(p.getMode())) {
                     if (p.getDeclaredType() instanceof PTFamily) {
                         newAssume = block.g.formConjunct(newAssume,
                                 ((PTFamily) p.getDeclaredType())
                                         .getConstraint());
                     }
-                }
+                }*/
             }
             PExp RP = block.finalConfirm.getConfirmExp();
             Map<PExp, PExp> newAssumeSubtitutions = new HashMap<>();
