015e6660b7fa03ae64971a47fa3fc033001d3218
Welchd1 <dtw.welch@gmail.com>
2015-12-26 14:48:41 -0500

    added a bunch of missing files

---
+++ compiler/src/edu/clemson/resolve/codegen/model/FacilityImplModule.java
@@ -0,0 +1,32 @@
+package edu.clemson.resolve.codegen.model;
+
+import org.rsrg.semantics.symbol.ModuleParameterSymbol;
+import org.rsrg.semantics.symbol.Symbol;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FacilityImplModule extends Module {
+    public String conceptName, definedMain;
+    @ModelElement public List<FacilityDef> facilities = new ArrayList<>();
+
+    public FacilityImplModule(String name, ModuleFile file) {
+        super(name, file);
+    }
+
+    public String getDefinedMain() {
+        for (FunctionDef f : funcImpls) {
+            if ( f.name.equalsIgnoreCase("main") ) {
+                return f.name;
+            }
+        }
+        return null;
+    }
+
+    //does nothing for impls. No module params or generics possible...
+    @Override public void addGettersAndMembersForModuleParameterSyms(
+            List<ModuleParameterSymbol> symbols) {}
+
+    @Override public void addOperationParameterModelObjects(
+            FunctionDef wrappedFunction) {}
+}
