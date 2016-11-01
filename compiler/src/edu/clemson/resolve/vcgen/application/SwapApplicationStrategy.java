53cc3229991d5af4b834d7bbd7b260e21b249d82
Welchd1 <dtw.welch@gmail.com>
2016-11-01 15:29:30 -0400

    progress

--- compiler/src/edu/clemson/resolve/vcgen/application/SwapApplicationStrategy.java
+++ compiler/src/edu/clemson/resolve/vcgen/application/SwapApplicationStrategy.java
@@ -1,5 +1,7 @@
 package edu.clemson.resolve.vcgen.application;
 
+import edu.clemson.resolve.proving.absyn.PExp;
+import edu.clemson.resolve.proving.absyn.PSymbol;
 import edu.clemson.resolve.vcgen.AssertiveBlock;
 import edu.clemson.resolve.vcgen.VCAssertiveBlock.VCAssertiveBlockBuilder;
 import edu.clemson.resolve.vcgen.stats.VCSwap;
@@ -9,22 +11,20 @@ import java.util.Deque;
 
 public class SwapApplicationStrategy implements VCStatRuleApplicationStrategy<VCSwap> {
 
-    //TODO: Todo, maybe make vcswapStat, vcwh
     @NotNull
     @Override
     public AssertiveBlock applyRule(@NotNull Deque<VCAssertiveBlockBuilder> accumulator,
                                     @NotNull VCAssertiveBlockBuilder block,
                                     @NotNull VCSwap stat) {
-       /* PExp workingConfirm = block.finalConfirm.getConfirmExp();
-        PExp swapLeft = stat.getStatComponents().get(0);
-        PExp swapRight = stat.getStatComponents().get(1);
-
-        PExp temp = new PSymbolBuilder((PSymbol)swapLeft).name("_t;").build();
+        PExp workingConfirm = block.finalConfirm.getConfirmExp();
+        PExp swapLeft = stat.getLeft();
+        PExp swapRight = stat.getRight();
+        PExp temp = new PSymbol.PSymbolBuilder((PSymbol)swapLeft).name("_t;").build();
 
         workingConfirm = workingConfirm.substitute(swapRight, temp);
         workingConfirm = workingConfirm.substitute(swapLeft, swapRight);
         workingConfirm = workingConfirm.substitute(temp, swapLeft);
-        block.finalConfirm(workingConfirm);*/
+        block.finalConfirm(workingConfirm);
         return block.snapshot();
     }
 
